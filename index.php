4834ddbc209732bedf6790495a45095d7f9d8c25
Irak Darek Ortiz Soqui <37721867+pirer48@users.noreply.github.com>
2021-08-27 15:39:21 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<title> The Soqui's WebPage</title>
+<body>
+    <hr>
+    <center><h1>
+        This is my site
+    </h1></center>
+    <hr>
+    <center></center>
+    <div>
+        <h2>This is me:</h2>
+        <img src="/Soqui.jpg" height="300px" widht="300px"/>
+        <h2>Im a student of software enginering. Now im in 7th semester.</h2>
+        <h2>I dont know what i have to write here but if you want to know something about me you can ask me to my mail: irakdarek@gmail.com</h2>
+        
+        
+    </div>
+</body>
+
+</html>
